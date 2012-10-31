5319a1359ee30e3410f97717c652b1c375edceeb
Pete Johns <paj-github@johnsy.com>
2012-11-01 09:08:10 +1100

    Tweak for firewalls
    Try ssh first, then git, then https.
    
    get_repos → get_starred_vim_repos

--- get_vim_bundles.py
+++ get_vim_bundles.py
@@ -7,7 +7,7 @@ import sys
 import urllib2
 
 
-def get_repos(user):
+def get_starred_vim_repos(user):
     url = 'https://api.github.com/users/{0}/starred'.format(user)
     result = urllib2.urlopen(url)
     stream = json.load(result)
@@ -35,24 +35,44 @@ def ensure_exists(bundle):
 
 
 def update_bundles(repos, bundles):
-    current_dir = os.getcwd()
     for repo in repos:
         name = repo['name']
         repo_dir = os.path.join(bundles, name)
         if os.path.isdir(repo_dir):
-            print 'Pulling {0} into {1}...'.format(name, bundles)
-            os.chdir(repo_dir)
-            subprocess.check_output(['git', 'pull', '--rebase'])
-            os.chdir(current_dir)
+            git_pull(name, repo_dir)
         else:
-            print 'Cloning {0} into {1}...'.format(name, bundles)
-            subprocess.check_output(['git', 'clone', repo['ssh_url'],
-                                     repo_dir])
+            git_clone(name, repo_dir, repo)
+
+
+def git_pull(name, repo_dir):
+    print 'Pulling {0} into {1}...'.format(name, repo_dir)
+    current_dir = os.getcwd()
+    os.chdir(repo_dir)
+    try:
+        subprocess.check_call(['git', 'pull', '--rebase'])
+    except subprocess.CalledProcessError, e:
+        print e
+    os.chdir(current_dir)
+
+
+def git_clone(name, repo_dir, repo):
+
+    def do_clone(url, repo_dir):
+        try:
+            subprocess.check_call(['git', 'clone', url, repo_dir])
+        except subprocess.CalledProcessError, e:
+            return False
+        return True
+
+    print 'Cloning {0} into {1}...'.format(name, repo_dir)
+    for url in 'ssh_url', 'git_url', 'clone_url':
+        if do_clone(repo[url], repo_dir):
+            break
 
 
 def main():
     username = get_username()
-    repos = get_repos(username)
+    repos = get_starred_vim_repos(username)
     bundle = get_bundle_directory()
     ensure_exists(bundle)
     update_bundles(repos, bundle)
